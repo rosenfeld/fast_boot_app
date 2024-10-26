ed21ee693b66b2e7d294190ed41a32c44ab72d3c
Rodrigo Rosenfeld Rosas <rodrigo@rosenfeld.page>
2024-10-26 12:00:00 -0300

    Fix User when Devise is not preloaded

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require "setup_devise"
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
