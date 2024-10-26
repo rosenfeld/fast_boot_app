c4b263de8815695d3edf330b69d789eb92898f6d
Rodrigo Rosenfeld Rosas <rodrigo@rosenfeld.page>
2024-10-26 12:00:00 -0300

    Move Devise initializer to lib/setup_devise.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
-  devise_for :users
+  devise_for :users if respond_to? :devise_for
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
