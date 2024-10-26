5c644e66631fc9c1c04769f3eae7753a35852f98
Rodrigo Rosenfeld Rosas <rodrigo@rosenfeld.page>
2024-10-26 12:00:00 -0300

    Profile Rails initialization

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,19 @@
-# Load the Rails application.
-require_relative "application"
+def generate_flamegraph(filename, &block)
+  return yield unless ENV["ENABLE_PROFILER"]
 
-# Initialize the Rails application.
-Rails.application.initialize!
+  require "stackprof"
+  require "json"
+
+  GC.disable
+  profile = StackProf.run(raw: true, &block)
+  GC.enable
+  File.write filename, JSON.unparse(profile)
+end
+
+generate_flamegraph("profile-initialize-application.json") do
+  # Load the Rails application.
+  require_relative "application"
+
+  # Initialize the Rails application.
+  Rails.application.initialize!
+end
