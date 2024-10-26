ea17dfae68d43f47ccda1eaf94c8da547e0070e4
Rodrigo Rosenfeld Rosas <rodrigo@rosenfeld.page>
2024-10-26 12:00:00 -0300

    Fix factory anti-pattern

--- spec/factories/fat_models.rb
+++ spec/factories/fat_models.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :fat_model do
     name { "MyText" }
 
-    factory :fat_model_subclass, class: FatModelSubclass do
+    factory :fat_model_subclass, class: "FatModelSubclass" do
     end
   end
 end
